532033cc302aa9906ecfaf3beca876a780f7c463
Joshua Nelson <jyn514@gmail.com>
2020-11-14 01:51:05 -0500

    Use DefPath for clean::Visibility, not clean::Path
    Visibility needs much less information than a full path, since modules
    can never have generics. This allows constructing a Visibility from only
    a DefId.
    
    Note that this means that paths are now normalized to their DefPath.
    In other words, `pub(self)` or `pub(super)` now always shows `pub(in
    path)` instead of preserving the original text.

--- src/librustdoc/clean/types.rs
+++ src/librustdoc/clean/types.rs
@@ -1527,12 +1527,18 @@ impl From<hir::PrimTy> for PrimitiveType {
     }
 }
 
-#[derive(Clone, PartialEq, Eq, Debug)]
+#[derive(Clone, Debug)]
 pub enum Visibility {
     Public,
     Inherited,
     Crate,
-    Restricted(DefId, Path),
+    Restricted(DefId, rustc_hir::definitions::DefPath),
+}
+
+impl Visibility {
+    crate fn is_public(&self) -> bool {
+        matches!(self, Visibility::Public)
+    }
 }
 
 #[derive(Clone, Debug)]
