cbd1b0b5e4af9c062ddead730465f3c739d2bfbb
Joshua Nelson <jyn514@gmail.com>
2021-06-05 08:53:15 -0400

    Give dynamically generated instructions on how to replicate errors during the build progress
    Example output:
    
    ```
    Checking stage0 rustdoc artifacts (x86_64-unknown-linux-gnu -> x86_64-unknown-linux-gnu)
        Checking rustdoc v0.0.0 (/home/joshua/rustc/src/librustdoc)
    error: expected item, found `/`
     --> src/librustdoc/lib.rs:1:1
      |
    1 | /
      | ^ expected item
    
    error: aborting due to previous error
    
    error: could not compile `rustdoc`
    
    To learn more, run the command again with --verbose.
    command did not execute successfully: "/home/joshua/rustc/build/x86_64-unknown-linux-gnu/stage0/bin/cargo" "check" "--target" "x86_64-unknown-linux-gnu" "-Zbinary-dep-depinfo" "-j" "8" "--release" "--manifest-path" "/home/joshua/rustc/src/tools/rustdoc/Cargo.toml" "--message-format" "json-render-diagnostics"
    expected success, got: exit status: 101
    note: failed while building bootstrap::check::Rustdoc
    help: to replicate this failure, run `./x.py check src/librustdoc`
    failed to run: /home/joshua/rustc/build/bootstrap/debug/bootstrap check src/librustdoc
    Build completed unsuccessfully in 0:00:01
    ```
    
    Happy to take suggestions on how to improve the error message :) right
    now it's getting lost in all the other output. Maybe it makes sense to
    use bold and colors?
    
    This works by storing the current step at all times in a global Mutex
    and registering an `at_exit` handler which looks up the current step.

--- src/bootstrap/builder.rs
+++ src/bootstrap/builder.rs
@@ -3,15 +3,17 @@ use std::cell::{Cell, RefCell};
 use std::collections::BTreeSet;
 use std::env;
 use std::ffi::OsStr;
-use std::fmt::Debug;
+use std::fmt::{self, Debug, Display};
 use std::fs;
 use std::hash::Hash;
 use std::ops::Deref;
 use std::path::{Path, PathBuf};
 use std::process::Command;
+use std::sync::Mutex;
 use std::time::{Duration, Instant};
 
 use build_helper::{output, t};
+use lazy_static::lazy_static;
 
 use crate::cache::{Cache, Interned, INTERNER};
 use crate::check;
@@ -59,6 +61,10 @@ pub trait Step: 'static + Clone + Debug + PartialEq + Eq + Hash {
 
     /// If true, then this rule should be skipped if --target was specified, but --host was not
     const ONLY_HOSTS: bool = false;
+
+    fn name(&self) -> &'static str {
+        std::any::type_name::<Self>()
+    }
 
     /// Primary function to execute this rule. Can call `builder.ensure()`
     /// with other steps to run those.
@@ -331,6 +337,26 @@ pub enum Kind {
     Doc,
     Install,
     Run,
+}
+
+impl Display for Kind {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Kind::*;
+        let s = match self {
+            Build => "build",
+            Check => "check",
+            Clippy => "clippy",
+            Fix => "fix",
+            Format => "fmt",
+            Test => "test",
+            Bench => "bench",
+            Dist => "dist",
+            Doc => "doc",
+            Install => "install",
+            Run => "run",
+        };
+        f.write_str(s)
+    }
 }
 
 impl<'a> Builder<'a> {
@@ -1533,12 +1559,28 @@ impl<'a> Builder<'a> {
         }
 
         let (out, dur) = {
+            let paths = S::should_run(ShouldRun::new(self)).paths;
+            let path = paths.iter().map(|pathset| pathset.path(self)).next();
+            let instructions = ReplicationStep {
+                name: step.name(),
+                cmd: self.kind,
+                path: path.expect("no paths for step"),
+            };
+            // NOTE: don't hold onto this guard, it will cause a deadlock if the current step calls `ensure` recursively.
+            let old_instructions = CURRENT_INSTRUCTIONS
+                .lock()
+                .expect("steps are not run in parallel")
+                .replace(instructions);
+
             let start = Instant::now();
             let zero = Duration::new(0, 0);
             let parent = self.time_spent_on_dependencies.replace(zero);
             let out = step.clone().run(self);
             let dur = start.elapsed();
             let deps = self.time_spent_on_dependencies.replace(parent + dur);
+
+            *CURRENT_INSTRUCTIONS.lock().expect("steps are not run in parallel") = old_instructions;
+
             (out, dur - deps)
         };
 
@@ -1554,6 +1596,28 @@ impl<'a> Builder<'a> {
         self.verbose(&format!("{}< {:?}", "  ".repeat(self.stack.borrow().len()), step));
         self.cache.put(step, out.clone());
         out
+    }
+}
+
+struct ReplicationStep {
+    cmd: Kind,
+    name: &'static str,
+    path: PathBuf,
+}
+
+lazy_static! {
+    static ref CURRENT_INSTRUCTIONS: Mutex<Option<ReplicationStep>> = Mutex::new(None);
+}
+
+pub(crate) extern "C" fn print_replication_steps() {
+    if let Some(step) = CURRENT_INSTRUCTIONS.lock().expect("mutex guard is dropped on panic").take()
+    {
+        println!("note: failed while building {}", step.name);
+        println!(
+            "help: to replicate this failure, run `./x.py {} {}`",
+            step.cmd,
+            step.path.display()
+        );
     }
 }
 
