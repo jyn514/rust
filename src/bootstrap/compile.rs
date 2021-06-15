282802b0bb1a2177c9f8971525dac8fe3d6ec905
Joshua Nelson <jyn514@gmail.com>
2021-06-15 07:32:23 -0400

    more progress
    - add stage override
    - switch more things to step_info

--- src/bootstrap/compile.rs
+++ src/bootstrap/compile.rs
@@ -515,6 +515,10 @@ impl Step for Rustc {
         });
     }
 
+    fn stage(&self, _builder: &Builder<'_>) -> u32 {
+        self.compiler.stage
+    }
+
     /// Builds the compiler.
     ///
     /// This will build the compiler for a particular stage of the build using
@@ -599,10 +603,11 @@ impl Step for Rustc {
             ));
         }
 
-        builder.info(&format!(
-            "Building stage{} compiler artifacts ({} -> {})",
-            compiler.stage, &compiler.host, target
-        ));
+        builder.step_info(&self);
+        // builder.info(&format!(
+        //     "Building stage{} compiler artifacts ({} -> {})",
+        //     compiler.stage, &compiler.host, target
+        // ));
         run_cargo(
             builder,
             cargo,
