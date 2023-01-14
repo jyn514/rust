7ebc8f58f164c480fd5ff5386ccd74e15e424247
Joshua Nelson <github@jyn.dev>
2023-01-14 08:30:23 +0000

    print version

--- collect_new_feature_versions.sh
+++ collect_new_feature_versions.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 for version in `seq 66 -1 49`; do
+    echo $version
     git checkout 1.$version.0 -- compiler/ library/ src/build_helper
     x t tidy | tee -a features_used_in_compiler_version.txt
 done
