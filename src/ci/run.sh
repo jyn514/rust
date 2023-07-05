8878db316b4f89fb2d3641a00811f3490b6a66c3
jyn <github@jyn.dev>
2023-07-05 14:12:52 -0500

    give a hard error if setting DOWNLOAD_RUSTC in a dist builder
    these should compile from source; i have reasonable confidence in download-rustc, but not so much that i want to allow shipping it in dist artifacts.

--- src/ci/run.sh
+++ src/ci/run.sh
@@ -106,6 +106,11 @@ if [ "$DEPLOY$DEPLOY_ALT" = "1" ]; then
   fi
 
   RUST_CONFIGURE_ARGS="$RUST_CONFIGURE_ARGS --set rust.codegen-backends=${CODEGEN_BACKENDS:-llvm}"
+
+  if [ "$DOWNLOAD_RUSTC" = 1 ]; then
+    echo "error: DOWNLOAD_RUSTC should not be set in dist builders!" >&2
+    exit 1
+  fi
 else
   # We almost always want debug assertions enabled, but sometimes this takes too
   # long for too little benefit, so we just turn them off.
